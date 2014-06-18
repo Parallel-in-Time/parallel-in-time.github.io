11076dbe1809ab4e4f90dfb9abba98cbe19d4fd0
Jenkins <jenkins@pint.fz-juelich.de>
2014-06-18 15:39:47 +0200

    Site updated at: 2014-06-18 13:39:47 UTC

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  $('article.post-content .content > p:first').addClass('lead');
-})
+    $('article.post-content .content > p:first').addClass('lead');
+    
+    $('div.reference > span.doi')
+        .wrapInner('<a href="http://dx.doi.org/'+this.innerHTML+'"></a>');
+    $('div.reference > span.arxivid')
+        .wrapInner('<a href="//arxiv.org/abs/'+this.innerHTML+'"></a>');
+    $('div.reference > span.mendeley').each(function() {
+        var link = this.innerHTML;
+        $(this).replaceWith('<a class="mendeley hidden-xs" role="button" href="'
+            + link
+            + '" title="Read on Mendeley" target="_blank">'
+            + '<i class="fa fa-search"></i></a>');
+    });
+    $('div.reference > a.mendeley').each(function() {
+        $(this).parent().prepend($(this).clone());
+        $(this).remove();
+    });
+});
